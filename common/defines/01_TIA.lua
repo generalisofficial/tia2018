6517643b06df663b82651b9d37ef960c8ece710a
Generalis <mavi-victor@hotmail.com>
2021-11-23 20:17:10 +0100

    Cock and ball torture episode 1

--- common/defines/01_TIA.lua
+++ common/defines/01_TIA.lua
@@ -7,8 +7,16 @@ NDefines.NDiplomacy.TENSION_TIME_SCALE_START_DATE = "2018.1.1.12"
 NDefines.NDiplomacy.TENSION_CB_WAR = 10
 
 NDefines.NCountry.MIN_SURRENDER_LIMIT = 0.1
-NDefines.NCountry.COUNTRY_LEADER_FEMALE_CHANCE = 0.25
+NDefines.NCountry.FEMALE_UNIT_LEADER_BASE_CHANCE = {
+    0.25, -- country leaders
+    0.0, -- army leaders
+    0.0, -- navy leaders
+    0.0, -- air leaders
+    1.0, -- operatives
+}
 
 NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 10}
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_TEX_MAX_SIZE = 1024
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_SMALL_TEX_MAX_SIZE = 512
 
 NDefines.NResistance.INITIAL_HISTORY_COMPLIANCE = 100
